790122b9e321df83c8bab02546e7bb48d2aa4cf4
andri <andriwicaksonost@gmail.com>
2019-10-16 15:43:01 +0700

    Getting names

--- js/app.js
+++ js/app.js
@@ -25,14 +25,24 @@ function generateNames(e) {
   const xhr = new XMLHttpRequest();
 
   //Open XHR Connection
-  xhr.open('GET', url, true);
+  xhr.open("GET", url, true);
 
   xhr.onload = function() {
-	  if(this.status === 200){
-		  const names = JSON.parse(this.responseText);
-		  console.log(names);
-	  }
-  }
+    if (this.status === 200) {
+      const names = JSON.parse(this.responseText);
+      console.log(names);
+      let html = "<h3>Generated Names</h3>";
+      html += '<ul class="list">';
+      names.forEach(name => {
+        html += `
+			<li>${name.name} ${name.surname}</li> 
+	 `;
+      });
+
+      html += "</ul>";
+	  document.querySelector("#result").innerHTML = html;
+    }
+  };
 
   xhr.send();
 
