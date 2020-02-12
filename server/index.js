bb9f314a970456241ffcabf11b81e140a3262e13
Andri Sedania <andri@sedaniainnovator.com>
2020-02-12 08:28:38 +0700

    32.280 - Working with Views

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const routes = require('./routes');
+const path = require('path');
 
 //Create the new server
 const app = express();
+
+//enable Pug
+app.set('view engine', 'pug');
+
+// add the views folder to project
+app.set('views', path.join(__dirname, './views'))
 
 //Listen For the homepage
 app.use('/', routes());
