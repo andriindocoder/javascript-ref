a0409562a208006b45444daa68284039bde53081
Andri Sedania <andri@sedaniainnovator.com>
2020-02-19 09:05:44 +0700

    32.284 - Passing Variables to the Views

--- server/index.js
+++ server/index.js
@@ -14,6 +14,13 @@ app.set('views', path.join(__dirname, './views'))
 // load the public asset folder
 app.use(express.static('public'));
 
+// get the current year
+app.use((req, res, next) => {
+	const date = new Date();
+	res.locals.currentYear = date.getFullYear();
+	return next();
+});
+
 //Listen For the homepage
 app.use('/', routes());
 
