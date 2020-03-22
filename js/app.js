f88bb67d70886869c43946154df274671bfd9536
Andri Sedania <andri@sedaniainnovator.com>
2020-03-22 15:30:46 +0700

    215 - Finishing the Project

--- js/app.js
+++ js/app.js
@@ -21,8 +21,19 @@ UI.searchForm.addEventListener('submit', (e) => {
 		const lyric = new API(artistName, songName);
 		lyric.queryAPI()
 			.then(data => {
-				let result = data.lyric.lyrics;
-				UI.resultDiv.textContent = result;
+				if(data.lyric.lyrics){
+					let result = data.lyric.lyrics;
+					UI.resultDiv.textContent = result;
+				} else {
+					// No results found
+					UI.messageDiv.innerHTML = 'No Lyrics Found';
+					UI.messageDiv.classList.add('error');
+					setTimeout(() => {
+						UI.messageDiv.innerHTML = '';
+						UI.messageDiv.classList.remove('error');
+						UI.searchForm.reset();
+					}, 3000);
+				}
 			});
 	}
 });
