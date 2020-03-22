26f2b831188f2aca3ddf1a7a9f59079d7cd324c7
Andri Sedania <andri@sedaniainnovator.com>
2020-03-22 15:01:41 +0700

    213 - Validating the Form

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
 import * as UI from './ui.js';
+
+UI.searchForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	//Read the form data
+	const artistName = UI.artistInput.value,
+		  songName = UI.songInput.value;
+
+	//Validate form
+	if(artistName === '' | songName === '') {
+		UI.messageDiv.innerHTML = 'All fields are mandatory';
+		UI.messageDiv.classList.add('error');
+		setTimeout(() => {
+			UI.messageDiv.innerHTML = '';
+			UI.messageDiv.classList.remove('error');
+		}, 3000);
+	} else {
+		//Query API
+	}
+});
