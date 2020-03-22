90c753a15d41286f81753026a86dcbb96004cccc
Andri Sedania <andri@sedaniainnovator.com>
2020-03-22 14:43:44 +0700

    212 - Importing the User Interface Elements

---
+++ js/app.js
@@ -0,0 +1,1 @@
+import * as UI from './ui.js';
---
+++ js/ui.js
@@ -0,0 +1,4 @@
+export const searchForm = document.querySelector('#search-form'),
+			 searchDiv = document.querySelector('#search'),
+			 resultDiv = document.querySelector('#result'),
+			 messageDiv = document.querySelector('#messages');
