0353ba1a57adfb93218544cd306822a01ece9c4c
Andri Sedania <andri@sedaniainnovator.com>
2020-03-25 21:31:46 +0700

    13.124 - Ajax and JSON Part 2

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 document.getElementById('button1').addEventListener('click', loadEmployee);
+document.getElementById('button2').addEventListener('click', loadEmployees);
 
 function loadEmployee() {
 	//Create the object
@@ -20,6 +21,35 @@ function loadEmployee() {
 				</ul>
 			`;
 			document.getElementById('employee').innerHTML = output;
+		}
+	}
+
+	xhr.send();
+}
+
+function loadEmployees() {
+	//Create the object
+	const xhr = new XMLHttpRequest();
+
+	//Open Connection
+	xhr.open('GET', 'employees.json', true);
+
+	//Execute
+	xhr.onload = function() {
+		if(this.status === 200) {
+			const employees = JSON.parse(this.responseText);
+			let output = '';
+			employees.forEach(function(employee) {
+				output += `
+					<ul>
+						<li>ID: ${employee.id}</li>
+						<li>Name: ${employee.name}</li>
+						<li>Company: ${employee.company}</li>
+						<li>Job: ${employee.job}</li>
+					</ul>
+				`;
+			});
+			document.getElementById('employees').innerHTML = output;
 		}
 	}
 
