c081634b3f40a07afad1a074ad6b72e44a30896f
Andri Sedania <andri@sedaniainnovator.com>
2020-03-25 20:57:25 +0700

    AjaxJSON base app
    13.123 - Ajax and JSON Part 1

--- app.js
+++ app.js
@@ -1,27 +1,76 @@
-document.getElementById('button').addEventListener('click', loadData);
+document.getElementById('button1').addEventListener('click', loadEmployee);
 
-function loadData() {
-	//Create new xML HTttp Request
-	const xhr = new XMLHttpRequest();
+document.getElementById('button2').addEventListener('click', loadEmployees);
 
-	// Open the connection
-	xhr.open('GET', 'data.txt', true);
 
-	// Ajax call execution
-	// xhr.onload = function() {
-	// 	if(this.status === 200) {
-	// 		document.getElementById('output').innerHTML = `<h1>${this.responseText}</h1>`;
-	// 	}
-	// }
-	xhr.onreadystatechange = function() {
-		/*
-			readyState 0 = Unsent; 1 = Opened; 2 = Received; 3 = Loading; 4 = Done;
-		*/
-		if(this.status === 200 && this.readyState === 4) {
-			document.getElementById('output').innerHTML = `<h1>${this.responseText}</h1>`;
-		}
-	}
 
-	//SEnd the request
-	xhr.send();
+// Display Single Employee
+function loadEmployee() {
+     // Create the object
+     const xhr = new XMLHttpRequest();
+
+     // Open the connection
+     xhr.open('GET', 'employee.json', true);
+
+     // Execute the function
+     xhr.onload = function() {
+          if(this.status === 200) {
+               // Get the response as an Object
+               const employee = JSON.parse( this.responseText );
+               
+               // Build the Template
+               const output = `
+                    <ul>
+                         <li>ID: ${employee.id}</li>
+                         <li>Name: ${employee.name}</li>
+                         <li>Company: ${employee.company}</li>
+                         <li>Job: ${employee.job}</li>
+                    </ul>
+               `;
+               // Prints the HTML
+               document.getElementById('employee').innerHTML = output;
+          }
+     }
+
+     // Send the request
+     xhr.send();
+
+}
+
+// Display all the Employees
+
+
+function loadEmployees() {
+     // Create the object
+     const xhr = new XMLHttpRequest();
+
+     // Open the connection
+     xhr.open('GET', 'employees.json', true);
+
+     // Execute the function
+     xhr.onload = function() {
+          if(this.status === 200) {
+               // Get the response as an Object
+               const employees = JSON.parse( this.responseText );
+               
+               let output = '';
+               employees.forEach(function(employee) {
+                    output += `
+                         <ul>
+                              <li>ID: ${employee.id}</li>
+                              <li>Name: ${employee.name}</li>
+                              <li>Company: ${employee.company}</li>
+                              <li>Job: ${employee.job}</li>
+                         </ul>
+                    `;
+               });
+
+               document.getElementById('employees').innerHTML = output;
+              
+          }
+     }
+
+     // Send the request
+     xhr.send();
+
 }
