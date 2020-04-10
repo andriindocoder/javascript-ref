c07f873d9ea6f812d73d72a0545fe04a1d7df4b2
Andri Sedania <andri@sedaniainnovator.com>
2020-04-10 15:38:47 +0700

    16.135 Loading a text from a file

---
+++ js/app.js
@@ -0,0 +1,15 @@
+document.getElementById('button1').addEventListener('click', loadTxt);
+
+function loadTxt() {
+	fetch('data.txt')
+	.then(function(response){
+		return response.text()
+	})
+	.then(function(data){
+		console.log(data);
+		document.getElementById('result').innerHTML = data;
+	})
+	.catch(function(error){
+		console.log(error);
+	})
+}
