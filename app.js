e47e3919b5e400631cf8b9c7d593f2e10c79fa65
Andri Sedania <andri@sedaniainnovator.com>
2020-04-10 15:11:48 +0700

    Promises

--- app.js
+++ app.js
@@ -1,37 +1,20 @@
-const cities = ['London', 'New York', 'Paris', 'Madrid', 'Berlin'];
+// Promises
+const applyDiscount = new Promise(function(resolve, reject) {
+	//Resolve when success
+	//Reject when failed
 
-//Inline callbacks
-cities.forEach(function(city) {
-	console.log(city);
+	const discount = true;
+
+	if(discount) {
+		resolve('Discount Applied');
+	} else {
+		reject('Discount Failed');
+	}
+
 });
 
-//Callback with function declaration
-function callback(city) {
-	console.log(city);
-}
-cities.forEach(callback);
-
-const countries = ['France', 'Spain', 'Portugal', 'Australia', 'US'];
-
-function newCountry(country, callback) {
-	setTimeout(function() {
-		countries.push(country);
-		//Execute the callback
-		callback();
-	}, 2000);
-}
-
-//Display the countries after 1 second
-function displayCountries() {
-	setTimeout(function() {
-		let html = '';
-		countries.forEach(function(country) {
-			html += `<li>${country}</li>`
-		});
-		document.body.innerHTML = html;
-	}, 1000);
-}
-
-newCountry('Germany', displayCountries);
-
-displayCountries();
+applyDiscount.then(function(result) {
+	console.log(result);
+}).catch(function(error){
+	console.log(error);
+})
