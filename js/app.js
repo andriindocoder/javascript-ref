6fe6c3401ad06c1dafed1420ca6d6a9a67b9518e
Andri Sedania <andri@sedaniainnovator.com>
2020-04-27 10:16:32 +0700

    27.218 - Adding the Main Selector for the UI

---
+++ js/app.js
@@ -0,0 +1,11 @@
+let DB;
+
+const form = document.querySelector('form'),
+		petName = document.querySelector('#pet-name'),
+		ownerName = document.querySelector('#owner-name'),
+		phone = document.querySelector('#phone'),
+		date = document.querySelector('#date'),
+		hour = document.querySelector('#hour'),
+		symptoms = document.querySelector('#symptoms'),
+		appointments = document.querySelector('#appointments'),
+		appointmentTitle = document.querySelector('#appointment-title'); 
