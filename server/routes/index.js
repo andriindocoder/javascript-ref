9738b77066f82f0638403564d08bad00d78bf7e2
Andri Sedania <andri@sedaniainnovator.com>
2020-05-07 17:42:33 +0700

    32. 293 - Querying the Database and Displaying the Travels Part 1

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,18 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
-module.exports = function() {
-	//homepage url
-	router.get('/', (req, res) => {
-		res.render('index', {
-			pageTitle: 'Home'
-		});
-	});
-	router.get('/about', (req, res) => {
-		res.render('about', {
-			pageTitle: 'About Us'
-		});
-	});
+const Travels = require('../models/Travels');
 
-	return router;
-}
+module.exports = function () {
+  //homepage url
+  router.get('/', (req, res) => {
+    res.render('index', {
+      pageTitle: 'Home',
+    });
+  });
+  router.get('/about', (req, res) => {
+    res.render('about', {
+      pageTitle: 'About Us',
+    });
+  });
+  router.get('/travels', (req, res) => {
+    Travels.findAll().then((travels) =>
+      res.render('travels', {
+        pageTitle: 'Upcoming Travels',
+        travels,
+      })
+    );
+  });
+
+  return router;
+};
