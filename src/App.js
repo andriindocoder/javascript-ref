42e6c70acdf3dd2178405424bd9a828640b72829
andri <andriwicaksonost@gmail.com>
2020-06-17 14:46:27 +0700

    Function to Class

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Todos from './components/Todos';
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Todos />
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner dash',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Meeting boss',
+        completed: false
+      }
+    ]
+  }
+  render() {
+    console.log(this.state.todos);
+    return (
+      <div className="App">
+        <Todos />
+      </div>
+    );
+  }
 }
 
 export default App;
