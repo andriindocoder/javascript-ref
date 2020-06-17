a39f69b25b2c63b7f610776a74871e36324634e3
andri <andriwicaksonost@gmail.com>
2020-06-17 14:59:49 +0700

    Cara 3 css

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,9 +2,21 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 
 export class TodoItem extends Component {
+    getStyle = () => {
+        if(this.props.todo.completed) {
+            return {
+                textDecoration: 'line-through'
+            }
+        } else {
+            return {
+                textDecoration: 'none'
+            }
+        }
+    }
+
     render() {
         return (
-            <div style={itemStyle}>
+            <div style={this.getStyle()}>
                 <p>{ this.props.todo.title }</p>
             </div>
         )
@@ -14,10 +26,6 @@ export class TodoItem extends Component {
 //PropTypes
 TodoItem.propTypes = {
     todo: PropTypes.object.isRequired
-}
-
-const itemStyle = {
-    backgroundColor: '#f4f4f4'
 }
 
 export default TodoItem
